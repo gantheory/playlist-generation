e6f01838ecb870b358e5e2a04b153a40a7c6e08e
Gary Shih <garyshih@KKBOXde-MacBook-Pro.local>
2017-08-23 14:52:24 +0800

    refactor

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,10 +1,7 @@
 # !/bin/bash
 cd data
-rm *.in
-rm *.ou
-rm train.tfrecords
+rm *.in *.ou train.tfrecords *filtered*
 echo 'processing raw data'
-python3 filter.py
 python3 data_utils.py
 echo 'processing TensorFlow Standard Format'
 python3 tf_format.py
