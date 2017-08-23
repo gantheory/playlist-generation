bd66f66e4781fa14c3bcc3eac3212015b1895692
Gary Shih <garyshih@KKBOXde-MacBook-Pro.local>
2017-08-23 14:52:58 +0800

    do not need filter.py now

--- data/filter.py
+++
@@ -1,7 +0,0 @@
-input_file = open('./raw_data.csv', 'r')
-output_file = open('./raw_data_filtered.csv', 'w')
-for line in input_file:
-    output_file.write(bytes(line, 'utf-8').decode('utf-8', 'igonre'))
-input_file.close()
-output_file.close()
-
